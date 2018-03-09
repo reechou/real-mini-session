190d7bc4aa9d27f144435c8f16c3bc3888a3b3a3
reezhou <reechou@gmail.com>
2018-03-09 23:02:32 +0800

    add

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"github.com/reechou/real-mini-session/config"
+	"github.com/reechou/real-mini-session/server"
+)
 
+func main() {
+	server.NewServer(config.NewConfig()).Run()
 }
